1e8438d6cb886fe63d7aaf03f776a7592859edaf
WooHyuk Jeong <jwh4807@gmail.com>
2016-06-24 22:05:36 +0900

    - Successfully installed socket.io module - Adapt socket.io on Client side using javascript - Tested simple chat server using socket.io emit method

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var htmlReader = require('fs');
+var socket_io = require('socket.io');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,7 +15,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-console.log(__dirname);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -26,9 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', routes);
 // Read main index file
 app.get('/', function(req, res){
-
-	//res.render('./views/index.html');
-	console.log("router");
+	console.log("http connection");
 	htmlReader.readFile('./views/index.html', function(error, data){
 
 		if(error)
